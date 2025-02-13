27967ee537570ecc79de9f05ceaa5661794ae8ca
Lukas.J.Han <lukas.j.han@gmail.com>
2025-02-13 15:47:20 +0900

    feat: add unprotected header type
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

--- packages/jades/src/type.ts
+++ packages/jades/src/type.ts
@@ -43,3 +43,37 @@ export type CommitmentOption = Array<{
   commId: string | CommitmentOIDs;
   commQuals?: Array<Record<string, unknown>>;
 }>;
+
+export type UnprotectedHeader = {
+  etsiU?: EtsiU;
+};
+
+export type EtsiU =
+  | [sigTst] // B-T profile
+  | [sigTst, XVal, rVal] // B-LT profile
+  | [sigTst, XVal, rVal, ArcTst]; // B-LTA profile
+
+export type TstToken = {
+  tstTokens: Array<{ val: string }>;
+};
+
+export type sigTst = {
+  sigTst: TstToken;
+};
+
+export type XVal = {
+  xVals: Array<{ x509Cert: string }>;
+};
+
+export type rVal = {
+  rVals: {
+    crlVals: Array<string>;
+    ocspVals: Array<string>;
+  };
+};
+
+export type ArcTst = {
+  arcTst: TstToken & {
+    canonAlg: string;
+  };
+};
